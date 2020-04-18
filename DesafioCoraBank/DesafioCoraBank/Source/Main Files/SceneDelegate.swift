f36c4d021d43d3201239d05e170a3f469ee7595c
Roger Sanoli <rogersanoli.app@gmail.com>
2020-04-18 19:11:46 -0300

    Setting up Fastlane pipeline

--- DesafioCoraBank/DesafioCoraBank/Source/Main Files/SceneDelegate.swift
+++ DesafioCoraBank/DesafioCoraBank/Source/Main Files/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
+    /*func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
@@ -51,7 +51,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-    }
+    }*/
 
 
 }
