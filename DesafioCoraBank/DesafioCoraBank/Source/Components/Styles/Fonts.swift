01ff219dc7ce9a414b7375b7c7729e8ae3ae608f
Roger Sanoli <rogersanoli.app@gmail.com>
2020-04-19 18:49:46 -0300

    Adding styles

---
+++ DesafioCoraBank/DesafioCoraBank/Source/Components/Styles/Fonts.swift
@@ -0,0 +1,47 @@
+//
+//  Fonts.swift
+//  DesafioCoraBank
+//
+//  Created by Roger Sanoli on 19/04/20.
+//  Copyright © 2020 RogerSanoli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIFont {
+    
+    private static let fontName = "Helvetica"
+    
+    static func cbRegular(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-Regular", size: size)!
+    }
+    
+    static func cbItalic(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-Italic", size: size)!
+    }
+    
+    static func cbLight(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-Light", size: size)!
+    }
+    
+    static func cbLightItalic(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-LightItalic", size: size)!
+    }
+    
+    static func cbMedium(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-Medium", size: size)!
+    }
+    
+    static func cbMediumItalic(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-MediumItalic", size: size)!
+    }
+    
+    static func cbBold(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-Bold", size: size)!
+    }
+    
+    static func cbBoldItalic(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "\(fontName)-BoldItalic", size: size)!
+    }
+}
---
+++ DesafioCoraBank/DesafioCoraBank/Source/Components/Styles/Margins.swift
@@ -0,0 +1,15 @@
+//
+//  Margins.swift
+//  DesafioCoraBank
+//
+//  Created by Roger Sanoli on 19/04/20.
+//  Copyright © 2020 RogerSanoli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension CGFloat {
+    static let cbDefaultMargin = 17.0
+    static let cbLineBreakMargin = 10.0
+}
