84e08bf8331179ad3e16fe02396d566f9921117f
Euclides Medeiros <euclidesmedeiros@MacBook-Pro-de-Euclides.local>
2021-03-09 11:26:15 -0300

    Iniciando projeto

---
+++ DesafioCora/Shared/DesafioCoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioCoraApp.swift
+//  Shared
+//
+//  Created by Euclides Medeiros on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DesafioCoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
