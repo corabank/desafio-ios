c2c46cd1b876e4006cb890f4fddf053376fb20f2
Euclides Medeiros <euclidesmedeiros@MacBook-Pro-de-Euclides.local>
2021-03-09 11:54:49 -0300

    removendo storyboard e iniciando desenvolvimento em viewCode

---
+++ DesafioCora/DesafioCora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesafioCora
+//
+//  Created by Euclides Medeiros on 09/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- DesafioCora/DesafioCoraUITests/DesafioCoraUITests.swift
+++ DesafioCora/DesafioCoraUITests/DesafioCoraUITests.swift
@@ -1,13 +1,13 @@
 //
-//  Tests_iOS.swift
-//  Tests iOS
+//  DesafioCoraUITests.swift
+//  DesafioCoraUITests
 //
 //  Created by Euclides Medeiros on 09/03/21.
 //
 
 import XCTest
 
-class Tests_iOS: XCTestCase {
+class DesafioCoraUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
