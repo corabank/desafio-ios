0688b2d29ef094720a92e0daccd0aaff82c905a6
Euclides Medeiros <euclidesmedeiros@MacBook-Pro-de-Euclides.local>
2021-03-19 17:25:13 -0300

    Refatorando view, viewModel e controller

---
+++ DesafioCora/DesafioCora/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  DesafioCora
+//
+//  Created by Euclides Medeiros on 19/03/21.
+//
+
+import Foundation
---
+++ DesafioCora/DesafioCora/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  DesafioCora
+//
+//  Created by Euclides Medeiros on 19/03/21.
+//
+
+import Foundation
