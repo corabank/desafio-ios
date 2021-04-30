0236a4d01b2c35d31d051e74ba5f8564fd824979
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2021-04-30 19:02:02 -0300

    OrdersList scroll gesture tests

---
+++ cora/Presentation/Feature/Orders/List/Extensions/Controller+withPanGestures.swift
@@ -0,0 +1,58 @@
+//
+//  Controller+withPanGestures.swift
+//  cora
+//
+//  Created by Lucas Silveira on 30/04/21.
+//
+
+import UIKit
+
+extension OrdersViewController: UIGestureRecognizerDelegate {
+    @objc func wasDragged(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        
+        switch recognizer.state {
+        case .began, .changed:
+            
+            let currentPosition = self.lastLocation.y + translation.y
+            
+            UIView.transition(
+                with: view,
+                duration: 0.05,
+                options: .curveEaseInOut,
+                animations: {
+                    self.view.transform = CGAffineTransform(
+                        translationX: 0,
+                        y: currentPosition)
+            })
+        case .ended:
+            lastLocation.y += translation.y
+            
+            if lastLocation.y <= topLimit {
+                UIView.transition(
+                    with: view,
+                    duration: 0.5,
+                    options: .curveEaseInOut,
+                    animations: {
+                        self.view.transform = CGAffineTransform(translationX: 0,
+                                                                y: self.topLimit)
+                })
+                lastLocation.y = self.topLimit
+            }
+
+            if lastLocation.y >= BOTTOM_LIMIT {
+                UIView.transition(
+                    with: view,
+                    duration: 0.5,
+                    options: .curveEaseInOut,
+                    animations: {
+                        self.view.transform = CGAffineTransform(translationX: 0,
+                                                                y: 0)
+                })
+                lastLocation.y = 0
+            }
+        default: break
+        }
+    }
+}
+
--- coraUITests/OrdersUITests.swift
+++ coraUITests/OrdersUITests.swift
@@ -26,7 +26,6 @@ class OrdersUITests: XCTestCase {
         let tableView = app.tables["ordersTableView"]
         let exists = NSPredicate(format: "exists == TRUE")
         expectation(for: exists, evaluatedWith: tableView) {
-            // If the table view exists, also check that it is enabled
             if tableView.isEnabled {
                 XCTAssert(tableView.cells.count >= 100)
                 
@@ -39,10 +38,36 @@ class OrdersUITests: XCTestCase {
                 }
                 return true
             } else {
-                // Do not fulfill the expectation since the table view is not enabled
                 return false
             }
         }
         waitForExpectations(timeout: 40, handler: nil)
+    }
+    
+    func testScrollingOrdersList() {
+        MakeLogin.run(app: app)
+        
+        let tableView = app.tables["ordersTableView"]
+        if app.waitForExistenceOfAll(elements: [tableView], for: 30) {
+            if tableView.isEnabled {
+                let firstCell = tableView.cells["order1"]
+                let lastCell = tableView.cells["order99"]
+                
+                while lastCell.isHittable == false {
+                    self.app.swipeUp(velocity: .fast)
+                }
+
+                XCTAssertFalse(firstCell.isHittable)
+                XCTAssertTrue(lastCell.isHittable)
+
+                while firstCell.isHittable == false {
+                    self.app.swipeDown(velocity: .fast)
+                }
+                
+                XCTAssertFalse(lastCell.isHittable)
+                XCTAssertTrue(firstCell.isHittable)
+                
+            }
+        }
     }
 }
