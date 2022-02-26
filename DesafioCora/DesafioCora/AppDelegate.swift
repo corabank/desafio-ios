2be30ff78a6961a46dd9490ef70c80087541f041
user205543 <user205543@74-80-245-240.ic2net.net>
2022-02-26 12:44:41 -0800

    Initial project structure

---
+++ DesafioCora/DesafioCora/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  DesafioCora
+//
+//  Created by user205543 on 2/26/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ DesafioCora/DesafioCora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioCora
+//
+//  Created by user205543 on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
