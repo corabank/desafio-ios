7008b12ff38f58a1f3b00473628d89947516b301
user205543 <user205543@74-80-245-240.ic2net.net>
2022-02-26 13:34:51 -0800

    Login Feature basic implementation

---
+++ DesafioCora/DesafioCora/Features/Login/LoginViewConroller.swift
@@ -0,0 +1,116 @@
+//
+//  LoginViewConroller.swift
+//  DesafioCora
+//
+//  Created by user205543 on 2/26/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    private lazy var logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.text = "AWESOME & Co."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var usernameField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Nome ou email"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Senha"
+        textField.isSecureTextEntry = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Acessar minha conta", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemPink
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Esqueci minha senha", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(forgotPasswordButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(logoImage)
+        view.addSubview(topLabel)
+        view.addSubview(usernameField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(forgotPasswordButton)
+    }
+    
+    private func setupConstraints() {
+        view.addConstraints([
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImage.widthAnchor.constraint(equalToConstant: 128),
+            logoImage.heightAnchor.constraint(equalToConstant: 128),
+            
+            topLabel.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 16),
+            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            usernameField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            usernameField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            usernameField.topAnchor.constraint(equalTo: topLabel.bottomAnchor, constant: 48),
+            usernameField.bottomAnchor.constraint(equalTo: passwordField.topAnchor, constant: -16),
+            
+            passwordField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            passwordField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            loginButton.bottomAnchor.constraint(equalTo: forgotPasswordButton.topAnchor, constant: -24),
+            
+            forgotPasswordButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            forgotPasswordButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    //MARK: Actions
+    
+    @objc
+    private func loginButtonTapped() {
+        present(TransactionsListViewController(), animated: true)
+    }
+    
+    @objc
+    private func forgotPasswordButtonTapped() {
+        
+    }
+}
---
+++ DesafioCora/DesafioCora/Features/TransactionsList/TransactionsListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionsListViewController.swift
+//  DesafioCora
+//
+//  Created by user205543 on 2/26/22.
+//
+
+import Foundation
+import UIKit
+
+class TransactionsListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
