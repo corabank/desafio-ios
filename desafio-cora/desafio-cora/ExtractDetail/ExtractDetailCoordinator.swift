73d03a1ec5d134cb0fc780cb6be413f74e2d918a
matheusvagnerperez <matheusvperez@gmail.com>
2022-04-14 19:40:29 -0300

    remove crop

--- desafio-cora/desafio-cora/ExtractDetail/ExtractDetailCoordinator.swift
+++ desafio-cora/desafio-cora/ExtractDetail/ExtractDetailCoordinator.swift
@@ -15,30 +15,6 @@ final class ExtractDetailCoordinator {
 
 }
 
-private extension ExtractDetailCoordinator {
-    func cropImage(_ inputImage: UIImage, toRect cropRect: CGRect, viewWidth: CGFloat, viewHeight: CGFloat) -> UIImage?
-    {
-        let imageViewScale = max(inputImage.size.width / viewWidth,
-                                 inputImage.size.height / viewHeight)
-
-        // Scale cropRect to handle images larger than shown-on-screen size
-        let cropZone = CGRect(x:cropRect.origin.x * imageViewScale,
-                              y:cropRect.origin.y * imageViewScale,
-                              width:cropRect.size.width * imageViewScale,
-                              height:cropRect.size.height * imageViewScale)
-
-        // Perform cropping in Core Graphics
-        guard let cutImageRef: CGImage = inputImage.cgImage?.cropping(to:cropZone)
-        else {
-            return nil
-        }
-
-        // Return image to UIImage
-        let croppedImage: UIImage = UIImage(cgImage: cutImageRef)
-        return croppedImage
-    }
-}
-
 // MARK: - ExtractDetailCoordinating
 extension ExtractDetailCoordinator: ExtractDetailCoordinating {
     func perform(action: ExtractDetailAction) {
