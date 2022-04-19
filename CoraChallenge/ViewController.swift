6e96ff3e3ec517d478862dfd20e931e9be0425fe
fchiarello <fellipe.chiarello@gmail.com>
2022-04-18 22:11:58 -0300

    initial commit

---
+++ CoraChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoraChallenge
+//
+//  Created by Fellipe Ricciardi Chiarello on 4/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
