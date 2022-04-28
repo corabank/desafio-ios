c2e8c094edcb9b6a3cf1ba5e9ac51e9b11ed914f
BMilliet <brunomilliet89@gmail.com>
2022-04-28 09:52:21 -0300

    dynamic keyboard

--- Modules/Login/Login/Form/CPFForm/View/CPFFormView.swift
+++ Modules/Login/Login/Form/CPFForm/View/CPFFormView.swift
@@ -7,6 +7,11 @@ final class CPFFormView: UIViewController {
     
     private var viewModel: CPFFormViewDelegate?
     
+    private let navTitle = "Login Cora"
+    private let welcome  = "Bem-vindo de volta!"
+    private let buttonTitle = "Próximo"
+    private let titleLabelString = "Qual seu CPF?"
+    
     private lazy var stack: UIStackView = {
         let stack: UIStackView = UIStackView(frame: .zero)
         stack.alignment = .center
@@ -15,15 +20,30 @@ final class CPFFormView: UIViewController {
         return stack
     }()
     
+    private lazy var stackEmbedded: UIStackView = {
+        let stack: UIStackView = UIStackView(frame: .zero)
+        stack.alignment = .center
+        stack.axis = .vertical
+        stack.spacing = Dimensions.verySmall
+        return stack
+    }()
+    
+    private lazy var footerStack: UIStackView = {
+        let stack: UIStackView = UIStackView(frame: .zero)
+        stack.alignment = .center
+        stack.axis = .vertical
+        return stack
+    }()
+    
     private lazy var navigation: NavigationBar = {
-        let nav = NavigationBar(title: "Extrato")
+        let nav = NavigationBar(title: navTitle)
         nav.set(delegate: self)
         return nav
     }()
 
     private lazy var welcomeLabel: UILabel = {
         let label = UILabel()
-        label.text = "Bem-vindo de volta!"
+        label.text = welcome
         label.numberOfLines = 0
         label.textAlignment = .justified
         label.textColor = Colors.darkGray
@@ -33,7 +53,7 @@ final class CPFFormView: UIViewController {
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Qual seu CPF?"
+        label.text = titleLabelString
         label.numberOfLines = 0
         label.textAlignment = .justified
         label.textColor = Colors.black
@@ -48,9 +68,14 @@ final class CPFFormView: UIViewController {
     
     private lazy var nextButton: RegularButton = {
         let button = RegularButton()
-        button.set(title: "Próximo", alignment: .left, style: .pink, icon: .arrow)
+        button.set(title: buttonTitle, alignment: .left, style: .pink, icon: .arrow)
         button.addTarget(self, action: #selector(nextActionCallback), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var keyboardHolder: UIView = {
+        let view = UIView()
+        return view
     }()
     
     required init?(coder: NSCoder) { return nil }
@@ -64,17 +89,33 @@ final class CPFFormView: UIViewController {
         nextButton.flash()
         viewModel?.inputCPF()
     }
+    
+    @objc
+    private func keyboardWillShow() {
+        footerStack.addArrangedSubview(keyboardHolder)
+    }
+    
+    @objc
+    private func keyboardWillHide() {
+        keyboardHolder.removeFromSuperview()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
 }
 
 extension CPFFormView: ViewCode {
     func setSubviews() {
-        view.addSubviews([stack, nextButton])
+        view.addSubviews([stack, footerStack])
         stack.addArrangedSubview(navigation)
         stack.addArrangedSubview(Spacer(size: Dimensions.small))
-        stack.addArrangedSubview(welcomeLabel)
-        stack.addArrangedSubview(titleLabel)
-        stack.addArrangedSubview(textField)
-        stack.addArrangedSubview(UIView(frame: .zero))
+        stack.addArrangedSubview(stackEmbedded)
+        stackEmbedded.addArrangedSubview(welcomeLabel)
+        stackEmbedded.addArrangedSubview(titleLabel)
+        stackEmbedded.addArrangedSubview(textField)
+        footerStack.addArrangedSubview(nextButton)
     }
     
     func setConstraints() {
@@ -83,20 +124,35 @@ extension CPFFormView: ViewCode {
                      leading: view.leadingAnchor,
                      trailing: view.trailingAnchor)
         
+        stackEmbedded.anchor(leading: stack.leadingAnchor,
+                             trailing: stack.trailingAnchor,
+                             paddingLeft: Dimensions.medium,
+                             paddingRight: Dimensions.medium)
+        
         welcomeLabel.setWidthEqual(to: stack)
         titleLabel.setWidthEqual(to: stack)
         
+        textField.size(height: 32)
+        textField.setWidthEqual(to: stackEmbedded)
+        
         nextButton.size(height: 50)
-        nextButton.anchor(leading: view.leadingAnchor,
-                          bottom: view.bottomAnchor,
-                          trailing: view.trailingAnchor,
-                          paddingBottom: Dimensions.mediumSmall,
-                          paddingLeft: Dimensions.medium,
-                          paddingRight: Dimensions.medium)
+        nextButton.setWidthEqual(to: footerStack)
+        footerStack.anchor(leading: view.leadingAnchor,
+                           bottom: view.bottomAnchor,
+                           trailing: view.trailingAnchor,
+                           paddingBottom: Dimensions.mediumSmall,
+                           paddingLeft: Dimensions.medium,
+                           paddingRight: Dimensions.medium)
+        keyboardHolder.size(height: 250)
     }
     
     func extraSetups() {
         view.backgroundColor = Colors.white
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
